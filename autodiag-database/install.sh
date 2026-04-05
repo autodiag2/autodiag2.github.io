d7fc1d8bd90ea909cf6215d7d9f7715730a1c77c
autodiag2 <autodiag@netcourrier.com>
2026-04-05 15:49:04 +0200

    move to sqlite database

--- autodiag-database/install.sh
+++ autodiag-database/install.sh
@@ -3,5 +3,4 @@ if ! [ -d "./database" ] ; then
     echo "must be runned as ./install.sh"
     exit 1
 fi
-cd database
-./manager/manager/compile_dtcs_json.py --out ../../site/public/tools/dtc_query/dtc.json
+cp database/data/ad_database.sqlite ../site/public/tools/dtc_query/
