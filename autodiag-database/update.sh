78f2a32f45faf88d57ee528ec6ae495114955bb5
autodiag2 <autodiag@netcourrier.com>
2026-07-25 14:31:59 +0200

    update database

--- autodiag-database/update.sh
+++ autodiag-database/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if ! [ -d "./database" ] ; then
-    echo "must be runned as ./install.sh"
+if ! [ -d "../site/public/tools/dtc_query/" ] ; then
+    echo "must be runned as ./update.sh"
     exit 1
 fi
+rm -f ../site/public/tools/dtc_query/ad_database.sqlite
 wget -P ../site/public/tools/dtc_query/ https://github.com/autodiag2/database/releases/latest/download/ad_database.sqlite
